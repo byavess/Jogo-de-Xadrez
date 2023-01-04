bd2d9b705219ced368e7738afb0b6e24413901e7
hp <byavess@gmail.com>
2023-01-03 23:08:59 -0300

    cincio do projeto

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,12 +1,12 @@
 package application;
 
+import boardGame.Board;
 import boardGame.Position;
 
 public class Programa {
     public static void main(String[] args) {
-        System.out.println("Hello Word!!");
-        Position pos = new Position(3,5);
-        System.out.println(pos);
+
+        Board board = new Board(8,8);
 
     }
 }
--- src/boardGame/Piece.java
+++ src/boardGame/Piece.java
@@ -1,4 +1,17 @@
 package boardGame;
 
+
+//TABULEIRO
 public class Piece {
+    protected Position position; // protected posição invisivel
+    private  Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+        position = null;
+    }
+    protected Board getBoard() {
+        return board;
+    }
+
 }
