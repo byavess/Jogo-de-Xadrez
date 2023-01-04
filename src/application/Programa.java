dcf1581cc428fd5919c2572b4dbfb31cd6bf269b
hp <byavess@gmail.com>
2023-01-03 22:30:29 -0300

    teste

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardGame.Position;
+
 public class Programa {
     public static void main(String[] args) {
         System.out.println("Hello Word!!");
+        Position pos = new Position(3,5);
+        System.out.println(pos);
 
     }
 }
